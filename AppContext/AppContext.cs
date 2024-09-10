3bf357f3c0a72e517ddee063b34acd775cde3ed6
Cedrick Mansoni <cedrick.k.mansoni@gmail.com>
2024-09-10 17:57:47 +0100

    Para garantir que os valores dos enums sejam armazenados como string no banco de dados usando Entity Framework Core, configurei o mapeamento no método OnModelCreating do DbContext

--- AppContext/AppContext.cs
+++ AppContext/AppContext.cs
@@ -1,4 +1,9 @@
 using System;
+using App_Imobiliaria_api.Models.imovel;
+using App_Imobiliaria_api.Models.lead;
+using App_Imobiliaria_api.Models.localizacao;
+using App_Imobiliaria_api.Models.mensagem;
+using App_Imobiliaria_api.Models.usuario;
 using Microsoft.EntityFrameworkCore;
 
 namespace App_Imobiliaria_api.AppContext;
@@ -9,6 +14,41 @@ public class AppContext : DbContext
     {
         optionsBuilder.UseNpgsql("Host = localhost; Port = 5432, User Id = PostgresDb; Database = db_imob; Password = dellgsa");
     }
+    /* USUARIO ========================================================================*/
+    public DbSet<Funcionario> TabelaFuncionarios { get; set; }
+    public DbSet<ClienteProprietario> TabelaClientesProprietarios {get; set; }
+    public DbSet<ClienteSolicitante> TabelaClientesSolicitantes {get; set; }
+    /* IMOVEL ========================================================================*/
+    public DbSet<Imovel> TabelaImovel { get; set; }
+    public DbSet<NaturezaImovel> TabelaNaturezaImovel { get; set; }
+    public DbSet<TipoImovel> TabelaTipoImovel { get; set; }
+    public DbSet<Foto> TabelaFoto { get; set; }
+    public DbSet<Publicacao> TabelaPublicacao { get; set; }
+    /* LOCALIZAÇÃO ========================================================================*/
+    public DbSet<Pais> TabelaPais { get; set; }
+    public DbSet<Provincia> TabelaProvincia { get; set; }
+    public DbSet<Municipio> TabelaMunicipio { get; set; }
+    public DbSet<Bairro> TabelaBairro { get; set; }
+    public DbSet<Rua> TabelaRua { get; set; }
+    public DbSet<Localizacao> TabelaLocalizacao { get; set; }
+    /* LEAD ========================================================================*/
+    public DbSet<Lead> TabelaLead { get; set; }
+    /* MENSAGEM ========================================================================*/
+    public DbSet<SolicitacaoCliente> TabelaSolicitacaoCliente { get; set; }
+    public DbSet<NotificarProprietario> TabelaNotificarProprietario { get; set; }
+    public DbSet<NotificarCliente> TabelaNotificarCliente { get; set; }
+    public DbSet<Chat> TabelaChat { get; set; }
+    /* ========================================================================*/
 
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Funcionario>().Property(f => f.Estado).HasConversion<string>();
+        modelBuilder.Entity<ClienteProprietario>().Property(c => c.Estado).HasConversion<string>();
+        modelBuilder.Entity<ClienteSolicitante>().Property(c => c.Estado).HasConversion<string>();
+        /* =======================================================================================*/
+        modelBuilder.Entity<Imovel>().Property(c => c.Estado).HasConversion<string>();
+        modelBuilder.Entity<Imovel>().Property(c => c.TipoPublicidade).HasConversion<string>();
+        /* =======================================================================================*/
+        modelBuilder.Entity<Lead>().Property(l => l.Estado).HasConversion<string>();
+    }
 }
